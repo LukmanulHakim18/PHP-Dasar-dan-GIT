4045efcfb4a902291208822fef7a80350adbf3b6
Marzuki <marzukiberg@gmail.com>
2021-03-18 22:31:56 +0700

    php jilid 3

--- index.php
+++ index.php
@@ -83,7 +83,7 @@ for (
     $jumlahGanjil = $jumlahGanjil + $i;
     // echo "ini perulangan ke " . $i . "<br>";
 }
-echo "jumlah angka ganjil dari 1 sampai 7 =" . $jumlahGanjil . "<br>";
+// echo "jumlah angka ganjil dari 1 sampai 7 =" . $jumlahGanjil . "<br>";
 
 //// if else
 
@@ -92,7 +92,119 @@ $bilangan1 = 6;
 $bilangan2 = 5;
 
 if ($bilangan1 > $bilangan2) {
-    echo $bilangan1 . " lebih besar dari" . $bilangan2;
+    // echo $bilangan1 . " lebih besar dari" . $bilangan2;
 } else {
-    echo $bilangan1 . " lebih Kecil dari" . $bilangan2;
+    // echo $bilangan1 . " lebih Kecil dari" . $bilangan2;
 }
+
+// PHP JILID 3
+// Local Scope
+// membuat sebuah fungsi
+function cetakAngka()
+{
+    $x = 5; // variabel dengan scope local
+    echo "Nilai variabel x pada fungsi adalah : $x";
+    echo "<br>";
+}
+cetakAngka(); // valid, gk error
+echo '<br>';
+// Menggunakan variabel x di luar fungsi akan menghasilkan error
+echo "Variable diluar fungsi adalah: $x";
+
+// Global Scope
+$x = 5; // Variabel dengan Scope Global memiliki jangkauan global
+echo '<br>';
+echo '<br>';
+echo "Variable diluar fungsi adalah: $x";
+
+echo '<br><br>';
+// Membuat global menjadi local scope
+$y = 10; // variabel dengan keyword global
+function cetakAngkaDua(){
+    global $y;
+    echo "Nilai dari variabel y adalah $y";
+}
+cetakAngkaDua();
+echo '<br><br>';
+echo "Nilai dari variabel y di luar fungsi adalah $y";
+
+// Static Variable
+
+function cetakAngkaIncreament() {
+    static $x = 0; // variabel ini nilainya dipertahankan
+    echo $x;
+    echo '<br>';
+    $x++; // disini dilakukan perubahan terhadap variabel x, yakni penambahan 1 angka
+}
+
+echo '<br><br>';
+cetakAngkaIncreament(); // dideklrasikan,
+cetakAngkaIncreament(); // var x dihapus, dan dideklarasikan lagi, pada static, nilai x tadi dipertahankan
+cetakAngkaIncreament(); // increment tetap berlanjut
+
+echo '<br><br>';
+// Anonymous Function
+$fungsiAnonymous = function(){
+    $x = 5;
+    echo "Variabel ini dicetak di dalam anonymous function $x";
+};
+$fungsiAnonymous();
+echo '<br><br>';
+
+// Anonymous Function dengan parameter
+$fungsiAnonymousDenganParameter = function($angka = 10){
+    echo "Nilai di dalam fungsi anonymous dengan parameter adalah $angka";
+};
+$fungsiAnonymousDenganParameter(5); // valid
+echo '<br>';
+$fungsiAnonymousDenganParameter(); // errpr, too few arguments, param harus ada
+
+
+echo '<br><br>';
+// Callback Function
+// membuat fungsi kalimat perintah
+function kalimatPerintah($kalimat){
+    return "$kalimat !!!";
+};
+// membuat fungsi kalimat tanya
+function kalimatTanya($kalimat){
+    return "$kalimat ???";
+};
+
+function cetakKalimat($format, $kalimat){
+    echo $format($kalimat); // parameter format berubah menjadi sebuah fungsi, callback function
+    echo '<br>';
+}
+
+cetakKalimat("kalimatPerintah", "Sholat dulu Yuk"); // mengembalikan nilai dari fungsi perintah
+cetakKalimat("kalimatTanya", "Apakah Anda sudah sholat"); // mengembalikan nilai dari fungsi tanya
+// cetakKalimat("fungsiIniTidakAda", "Fungsi ini tidak ada"); // error, call to undefined function
+
+echo '<br>';
+// Penggunaan fungsi bawaan die()
+$z = 20;
+// echo "Kode ini dieksekusi, nilai z = $z";
+// die(); // kode berikutnya tidak akan dieksekusi
+// echo "Kode ini tidak dieksekusi, nilai z = $z";
+// die dengan parameter pesan
+fopen("diary.txt", "r") or die("File gagal dimuat.");
+echo "<br>Ini akan dieksekusi jika fopen berhasil memuat file";
+
+echo '<br><br>';
+// Penggunaan var_dump();
+$angka = 7;
+$arrayAngka = [1, 2, 3];
+$huruf = 'ABC';
+echo $angka; // 7, informasi kurang detail (hanya untuk user/mode produksi)
+echo '<br><br>';
+var_dump($angka);
+echo '<br><br>';
+// echo $arrayAngka;
+var_dump($arrayAngka);
+// die(); // var_dump biasa disandingkan dengan die untuk mempermudah developer
+
+echo '<br><br>';
+// var_dump dapat menerima lebih dari satu parameter
+echo '<pre>';
+var_dump($angka, $arrayAngka, $huruf);
+echo '</pre>';
